cc4e93f51094eecc351c960b694f032f424f5878
willchapin <wrchapin@gmail.com>
2013-11-18 16:19:23 -0500

    separate clients and server in chat server class

--- ChatServerEventLoop/ChatServer.rb
+++ ChatServerEventLoop/ChatServer.rb
@@ -12,36 +12,42 @@ class ChatServer
   def initialize
     @server = Server.new(TCPServer.new(9393))
     set_listeners(@server)
-    @users = [@server]
+    @clients = []
     start_server
   end
 
+  def users
+    [@server] + @clients
+  end
   
   def set_listeners(server)
     server.on(:accept_new_user) do |client|
-      @users << client 
-      client.to_io.write_nonblock("Welcome to the Chatroom.\nType 'exit' to leave.\n")
+      @clients << client 
+      client.socket.write_nonblock("Welcome to the Chatroom.\nType 'exit' to leave.\n")
       register(client)
     end
   end
   
   def register(client)
-    client.on(:message) do |user|
-      message = user.to_io.read_nonblock(1000)
+    client.on(:message) do |current_client|
+      message = current_client.socket.read_nonblock(1000)
       if message.match(/exit/)
-        user.close
-        @users.delete(user)
+        sign_off(current_client)
       else
-        @users[1..-1].each do |user|
-          user.to_io.write_nonblock(message)
-        end
+        clients = @clients.select{ |client| client != current_client }
+        clients.each { |client| client.socket.write_nonblock(message) }
       end
     end
   end
 
+  def sign_off(client)
+    client.close
+    @clients.delete(client)
+  end
+
   def start_server
     loop do
-      readables, _ = IO.select(@users)
+      readables, _ = IO.select(users)
       readables.each do |socket|
         socket.handle_read
       end
